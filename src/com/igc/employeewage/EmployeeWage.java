275c6afaf22e595cf785d853c893561f8b59980e
TechnoSpaceIT <technospaceitclasses@gmail.com>
2023-04-21 20:55:06 +0530

    [ADDED]:[Tahir] Employee Wage Project Setup

---
+++ src/com/igc/employeewage/EmployeeWage.java
@@ -0,0 +1,12 @@
+package com.igc.employeewage;
+
+public class EmployeeWage {
+
+	public static void main(String[] args) {
+		
+		//Welcome message for user
+		System.out.println("Welcome to Employee Wage Computation Program");
+		
+	}
+
+}
