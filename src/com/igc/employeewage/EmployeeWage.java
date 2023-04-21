0efda6f4bd5aa3ec0c2d0058a2d9be2ed11a106d
TechnoSpaceIT <technospaceitclasses@gmail.com>
2023-04-21 21:18:52 +0530

    [MODIFIED]:[Tahir] Check for Employee Part time or Full Time

--- src/com/igc/employeewage/EmployeeWage.java
+++ src/com/igc/employeewage/EmployeeWage.java
@@ -11,10 +11,13 @@ public class EmployeeWage {
 		
 		int attendance;
 		int daily_wage = 0;
+		int empType;
 		
 		final int IS_ABSENT = 0;
 		final int WAGE_PER_HR = 20;
-		final int FULL_DAY_HR = 8;
+		final int FULL_TIME_HR = 8;
+		final int PART_TIME_HR = 4;
+		final int IS_PART_TIME = 0;
 		
 		Random random = new Random();
 		
@@ -25,7 +28,15 @@ public class EmployeeWage {
 			System.out.println("Employee is Absent");
 		} else {
 			System.out.println("Employee is Present");
-			daily_wage = WAGE_PER_HR * FULL_DAY_HR;
+			
+			empType = random.nextInt(2);
+			if(empType == IS_PART_TIME) {
+				System.out.println("Employee is Part Time");
+				daily_wage = WAGE_PER_HR * PART_TIME_HR;
+			} else {
+				System.out.println("Employee is Full Time");
+				daily_wage = WAGE_PER_HR * FULL_TIME_HR;
+			}
 		}
 		System.out.println("Daily Wage = "+ daily_wage);
 	}
