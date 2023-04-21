aab85a1c8dbef9956fc85c77c551934ebb05c7df
TechnoSpaceIT <technospaceitclasses@gmail.com>
2023-04-21 21:02:06 +0530

    [MODIFIED]:[Tahir] Check for Employee Attendance

--- src/com/igc/employeewage/EmployeeWage.java
+++ src/com/igc/employeewage/EmployeeWage.java
@@ -1,5 +1,7 @@
 package com.igc.employeewage;
 
+import java.util.Random;
+
 public class EmployeeWage {
 
 	public static void main(String[] args) {
@@ -7,6 +9,19 @@ public class EmployeeWage {
 		//Welcome message for user
 		System.out.println("Welcome to Employee Wage Computation Program");
 		
+		int attendance;
+		final int IS_ABSENT = 0;
+		
+		Random random = new Random();
+		
+		attendance = random.nextInt(2);
+		System.out.println("Attendance="+attendance);
+		
+		if(attendance == IS_ABSENT) {
+			System.out.println("Employee is Absent");
+		} else {
+			System.out.println("Employee is Present");
+		}
 	}
 
 }
