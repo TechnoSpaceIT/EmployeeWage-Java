80084c4a71f23698165cee36b86c9358f512bd1f
TechnoSpaceIT <technospaceitclasses@gmail.com>
2023-04-24 21:31:16 +0530

    [MODIFIED]:[Tahir] Calculating till 20 Days or 100 Hr

---
+++ src/com/igc/employeewage/Constants.java
@@ -0,0 +1,10 @@
+package com.igc.employeewage;
+
+public class Constants {
+	final static int IS_ABSENT = 0;
+	final static int WAGE_PER_HR = 20;
+	final static int FULL_TIME_HR = 8;
+	final static int PART_TIME_HR = 4;
+	final static int IS_PART_TIME = 0;
+	final static int IS_FULL_TIME = 1;
+}
--- src/com/igc/employeewage/EmployeeWage.java
+++ src/com/igc/employeewage/EmployeeWage.java
@@ -13,42 +13,40 @@ public class EmployeeWage {
 		int daily_wage = 0;
 		int month_wage = 0;
 		int empType;
-		
-		final int IS_ABSENT = 0;
-		final int WAGE_PER_HR = 20;
-		final int FULL_TIME_HR = 8;
-		final int PART_TIME_HR = 4;
-		final int IS_PART_TIME = 0;
-		final int IS_FULL_TIME = 1;
+		int total_hr = 0;
+		int days = 1;
 		
 		Random random = new Random();
 		
-		for(int i = 1; i <= 20; i++) {
-			System.out.println("Day = "+i);
+		while(days != 21 && total_hr < 100) {
+			System.out.println("Day = "+days+"   Total Hr="+total_hr);
 			daily_wage = 0;
 			attendance = random.nextInt(2);
 			System.out.println("Attendance="+attendance);
 			
-			if(attendance == IS_ABSENT) {
+			if(attendance == Constants.IS_ABSENT) {
 				System.out.println("Employee is Absent");
 			} else {
 				System.out.println("Employee is Present");
 				
 				empType = random.nextInt(2);
 				switch(empType) {
-					case IS_PART_TIME:
+					case Constants.IS_PART_TIME:
 						System.out.println("Employee is Part Time");
-						daily_wage = WAGE_PER_HR * PART_TIME_HR;
+						daily_wage = Constants.WAGE_PER_HR * Constants.PART_TIME_HR;
+						total_hr = total_hr + Constants.PART_TIME_HR;
 					break;
-					case IS_FULL_TIME:
+					case Constants.IS_FULL_TIME:
 						System.out.println("Employee is Full Time");
-						daily_wage = WAGE_PER_HR * FULL_TIME_HR;
+						daily_wage = Constants.WAGE_PER_HR * Constants.FULL_TIME_HR;
+						total_hr = total_hr + Constants.FULL_TIME_HR;
 					break;
 				}
 			}
 			System.out.println("Daily Wage = "+ daily_wage);
 			month_wage = month_wage + daily_wage;
 			System.out.println("###########################");
+			days++;
 		}
 		System.out.println("Total Monthly Wage = "+ month_wage);
 	}
