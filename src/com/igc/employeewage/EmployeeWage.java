a1ed71bb676814de6c38181a8c3ee450a225c52b
TechnoSpaceIT <technospaceitclasses@gmail.com>
2023-04-24 21:00:28 +0530

    [MODIFIED]:[Tahir] Code Modification with Switch Case

--- src/com/igc/employeewage/EmployeeWage.java
+++ src/com/igc/employeewage/EmployeeWage.java
@@ -18,6 +18,7 @@ public class EmployeeWage {
 		final int FULL_TIME_HR = 8;
 		final int PART_TIME_HR = 4;
 		final int IS_PART_TIME = 0;
+		final int IS_FULL_TIME = 1;
 		
 		Random random = new Random();
 		
@@ -30,12 +31,15 @@ public class EmployeeWage {
 			System.out.println("Employee is Present");
 			
 			empType = random.nextInt(2);
-			if(empType == IS_PART_TIME) {
-				System.out.println("Employee is Part Time");
-				daily_wage = WAGE_PER_HR * PART_TIME_HR;
-			} else {
-				System.out.println("Employee is Full Time");
-				daily_wage = WAGE_PER_HR * FULL_TIME_HR;
+			switch(empType) {
+				case IS_PART_TIME:
+					System.out.println("Employee is Part Time");
+					daily_wage = WAGE_PER_HR * PART_TIME_HR;
+				break;
+				case IS_FULL_TIME:
+					System.out.println("Employee is Full Time");
+					daily_wage = WAGE_PER_HR * FULL_TIME_HR;
+				break;
 			}
 		}
 		System.out.println("Daily Wage = "+ daily_wage);
